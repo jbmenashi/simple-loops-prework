c07756f4ae86875f16edca7b49572c807a5b3390
Jacob Menashi <jbmenashi@gmail.com>
2018-09-26 19:40:06 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -38,7 +38,7 @@ end
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   range = 1..number_of_times
-  for item in range do
+  for item in range
     puts phrase
   end
 end
